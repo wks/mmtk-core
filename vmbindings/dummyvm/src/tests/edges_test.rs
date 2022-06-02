e809d1909bccbca9c18159f4bae76e94725f2ad8
Kunshan Wang <wks1986@gmail.com>
2022-06-02 15:16:01 +0800

    Add a test case for enum-based edges.

--- vmbindings/dummyvm/src/tests/edges_test.rs
+++ vmbindings/dummyvm/src/tests/edges_test.rs
@@ -7,7 +7,7 @@ use mmtk::{
 };
 
 use crate::{
-    edges::{CompressedOopEdge, OffsetEdge, ValueEdge},
+    edges::{CompressedOopEdge, DummyVMEdge, OffsetEdge, ValueEdge},
     tests::fixtures::{Fixture, TwoObjects},
 };
 
@@ -116,5 +116,46 @@ pub fn load_value() {
         let objref = edge.load();
 
         assert_eq!(objref, fixture.objref1);
+    });
+}
+
+#[test]
+pub fn mixed() {
+    const OFFSET: usize = 48;
+
+    FIXTURE.with_fixture(|fixture| {
+        let addr1 = fixture.objref1.to_address();
+        let addr2 = fixture.objref2.to_address();
+
+        let mut slot1: Atomic<ObjectReference> = Atomic::new(fixture.objref1);
+        let mut slot3: Atomic<Address> = Atomic::new(addr1 + OFFSET);
+
+        let edge1 = SimpleEdge::from_address(Address::from_ref(&mut slot1));
+        let edge3 = OffsetEdge::new_with_offset(Address::from_ref(&mut slot3), OFFSET);
+        let edge4 = ValueEdge::new(fixture.objref1);
+
+        let de1 = DummyVMEdge::Simple(edge1);
+        let de3 = DummyVMEdge::Offset(edge3);
+        let de4 = DummyVMEdge::Value(edge4);
+
+        let edges = vec![de1, de3, de4];
+        for (i, edge) in edges.iter().enumerate() {
+            let objref = edge.load();
+            assert_eq!(objref, fixture.objref1, "Edge {} is not properly loaded", i);
+        }
+
+        let mutable_edges = vec![de1, de3];
+        for (i, edge) in mutable_edges.iter().enumerate() {
+            edge.store(fixture.objref2);
+            let objref = edge.load();
+            assert_eq!(
+                objref, fixture.objref2,
+                "Edge {} is not properly loaded after store",
+                i
+            );
+        }
+
+        assert_eq!(slot1.load(Ordering::SeqCst), fixture.objref2);
+        assert_eq!(slot3.load(Ordering::SeqCst), addr2 + OFFSET);
     });
 }
