6aa747d15496fceaf135db9f55b6f44b9a84c9ed
Kunshan Wang <wks1986@gmail.com>
2024-08-01 16:31:23 +0800

    WIP: Refactor for byte/word range visiting, too.

--- src/util/metadata/side_metadata/ranges.rs
+++ src/util/metadata/side_metadata/ranges.rs
@@ -37,15 +37,115 @@ pub enum BitByteRange {
     },
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum ByteWordRange {
-    Words {
-        start: Address,
-        end: Address,
-    },
-    BytesInWord {
-        start: Address,
-        end: Address,
-    },
+/// Iterate over a range of bits in a bitmap.
+///
+/// This method is primarily used for iterating side metadata for a data address range. As we cannot
+/// guarantee that the data address range can be mapped to whole metadata bytes, we have to deal
+/// with visiting only a bit range in a metadata byte.
+///
+/// The bit range starts at the bit at index `bit_start` in the byte at address `byte_start`, and
+/// ends at (but does not include) the bit at index `bit_end` in the byte at address `byte_end`.
+///
+/// Arguments:
+/// * `forwards`: If true, we iterate forwards (from start/low address to end/high address).
+///               Otherwise, we iterate backwards (from end/high address to start/low address).
+/// * `visitor`: The callback that visits ranges of bits or bytes.  It returns whether the itertion
+///   is early terminated.
+///
+/// Returns true if we iterate through every bits in the range. Return false if we abort iteration
+/// early.
+pub fn iterate_meta_bits<V>(
+    byte_start: Address,
+    bit_start: u8,
+    byte_end: Address,
+    bit_end: u8,
+    forwards: bool,
+    visitor: &mut V,
+) -> bool
+where
+    V: FnMut(BitByteRange) -> bool,
+{
+    trace!(
+        "iterate_meta_bits: {} {}, {} {}",
+        byte_start,
+        bit_start,
+        byte_end,
+        bit_end
+    );
+
+    // Start/end is the same, we don't need to do anything.
+    if byte_start == byte_end && bit_start == bit_end {
+        return false;
+    }
+
+    // visit whole bytes
+    if bit_start == 0 && bit_end == 0 {
+        return visitor(BitByteRange::Bytes {
+            start: byte_start,
+            end: byte_end,
+        });
+    }
+
+    if byte_start == byte_end {
+        // Visit bits in the same byte between start and end bit
+        return visitor(BitByteRange::BitsInByte {
+            addr: byte_start,
+            bit_start: bit_start as usize,
+            bit_end: bit_end as usize,
+        });
+    } else if byte_start + 1usize == byte_end && bit_end == 0 {
+        // Visit bits in the same byte after the start bit (between start bit and 8)
+        return visitor(BitByteRange::BitsInByte {
+            addr: byte_start,
+            bit_start: bit_start as usize,
+            bit_end: 8usize,
+        });
+    } else {
+        // We cannot let multiple closures capture `visitor` mutably at the same time, so we
+        // pass the visitor in as `v` every time.
+
+        // update bits in the first byte
+        let visit_start = |v: &mut V| {
+            v(BitByteRange::BitsInByte {
+                addr: byte_start,
+                bit_start: bit_start as usize,
+                bit_end: 8usize,
+            })
+        };
+
+        // update bytes in the middle
+        let visit_middle = |v: &mut V| {
+            let start = byte_start + 1usize;
+            let end = byte_end;
+            if start < end {
+                // non-empty middle range
+                v(BitByteRange::Bytes { start, end })
+            } else {
+                // empty middle range
+                false
+            }
+        };
+
+        // update bits in the last byte
+        let visit_end = |v: &mut V| {
+            v(BitByteRange::BitsInByte {
+                addr: byte_end,
+                bit_start: 0 as usize,
+                bit_end: bit_end as usize,
+            })
+        };
+
+        // Update each segments.
+        if forwards {
+            visit_start(visitor) || visit_middle(visitor) || visit_end(visitor)
+        } else {
+            visit_end(visitor) || visit_middle(visitor) || visit_start(visitor)
+        }
+    }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ByteWordRange {
+    Words { start: Address, end: Address },
+    BytesInWord { start: Address, end: Address },
+}
